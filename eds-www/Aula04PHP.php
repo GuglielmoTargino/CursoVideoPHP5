9b1a024af81bb0d03f099f8786b80937ad8aab52
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2022-08-20 10:39:17 -0300

    Aula04-PHP5
    Trabalhando com as variáveis. $num, $nome e echo.

---
+++ eds-www/Aula04PHP.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>Aula1_Teste_PHP</title>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+   
+
+  
+
+</head>
+
+
+
+
+<body>
+    <div>
+    <h1>Tô chegando! Mundo! Olá PHP!</h1>
+    <h2>Meus primeiros passos em PHP</h2>
+
+    <?php
+    $num=45;
+    $nome="Guglielmo_Targino";
+    
+     echo  $nome." tem  ". $num." anos!";
+    ?>
+
+</div>
+
+        
+</body>
+
+
+
+
+
+
+
+</html>
---
+++ eds-www/meuprimeirositePHP.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>Aula1_Teste_PHP</title>
+    <style>
+        h1{
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-size: 22pt;
+            color: blueviolet;
+            text-shadow: 2px 2px 4px green;
+        }
+
+        h2{
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            font-size: 20pt;
+            color: brown;
+            text-shadow: 3pt 3pt 3pt yellow;
+        }
+
+    </style>
+
+  
+
+</head>
+
+
+
+
+<body>
+    <h1>Tô chegando! Mundo! Olá PHP!</h1>
+    <h2>Meus primeiros passos em PHP</h2>
+
+    <?php
+    
+     echo  "<h2> comecando o php </h2>";
+    ?>
+
+    <script>
+       /* alert(" Ola mundo");*/
+        document.write(" Oi mundo! ola! Hoje é dia" + Date());
+    </script>
+    
+    
+
+        
+</body>
+
+
+
+
+
+
+
+</html>
