68834ce9f944d1565624e353f06ff557477cb6bb
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2022-09-05 19:22:49 -0300

    Aula13LaçoFor

---
+++ eds-www/aula13for.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>Aula1_Teste_PHP</title>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+
+  
+  
+
+</head>
+
+
+
+
+
+<body>
+    <div>
+        <h1>Tô chegando! Mundo! Olá PHP!</h1>
+        <h2>Meus primeiros passos em PHP</h2>
+
+        <?php
+        /*$c=$_GET["num"];*/
+        $c=0;
+        
+        for($c=1;$c<15;$c+=2){
+          echo " $c";
+        
+           
+        }
+
+       
+    
+    
+    
+    
+        ?>
+        
+      
+
+        
+
+        <p><a href="aula13.html" class="botao" > Clique aqui para voltar</a></p>
+
+
+    </div>
+
+        
+</body>
+</html>
