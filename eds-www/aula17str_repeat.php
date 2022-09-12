c3ce0e412954791e1ce4f607ecc71df93ab39308
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2022-09-12 17:38:50 -0300

    aula17e18Vetores

---
+++ eds-www/aula17str_repeat.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>CursoVideo_Aulas_PHP</title>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+
+  
+  
+
+</head>
+
+
+
+
+
+<body>
+    <div>
+        
+
+        <?php
+        $nome="Guglielmo";
+
+        $txt=str_repeat($nome,5);
+
+        echo " nome é $txt";
+           
+        ?>
+        
+      
+
+        
+
+        <!--<p><a href="aula14.html" class="botao" > Clique aqui para voltar</a></p>-->
+
+
+    </div>
+
+        
+</body>
+</html>
---
+++ eds-www/aula18vetores.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title>CursoVideo_Aulas_PHP</title>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+
+  
+  
+
+</head>
+
+
+
+
+
+<body>
+    <div>
+        
+
+        <?php
+        $nome=array(5,6,2,3);
+       print_r($nome);
+           
+        ?>
+        
+      
+
+        
+
+        <!--<p><a href="aula14.html" class="botao" > Clique aqui para voltar</a></p>-->
+
+
+    </div>
+
+        
+</body>
+</html>
